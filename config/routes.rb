88c77c74203ad8e493ff20511a41e1a54985e702
Atila <atilafb77@gmail.com>
2022-02-15 16:33:53 -0300

    add show page to specific user and created a link to this page from index.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'users' => 'users#index'
-  get 'users' => 'users#show'
   get 'users' => 'users#new'
   post 'users' => 'users#create'
   get 'users' => 'users#edit'
   put 'users' => 'users#update'
   delete 'users' => 'users#destroy'
+  get 'users/:id' => 'users#show', as: :user
 end
