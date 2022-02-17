854b4c13275005bc667ed1d4e17426988c44e805
Atila <atilafb77@gmail.com>
2022-02-17 13:36:43 -0300

    fix: tests and use inclusion to phone_kind validation

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -2,7 +2,7 @@ class Phone < ApplicationRecord
   belongs_to :contact
 
   validates :number, presence: true, uniqueness: { scope: :contact_id }
-  validates :phone_kind, presence: true
+  validates :phone_kind, inclusion: { in: ['casa', 'trabalho', 'outro'] }
   validates :is_main, uniqueness: { scope: :contact_id, conditions: lambda {
                                                                       where(is_main: true)
                                                                     } }
--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Phone, type: :model do
-  subject(:new_phone) { described_class.new(contact:, number: '11112222', phone_kind: 'Casa', is_main: true) }
+  subject(:new_phone) { described_class.new(contact:, number: '11112222', phone_kind: 'casa', is_main: true) }
 
   let(:user) do
     User.create(name: 'Jose', age: 18, biography: 'A long history')
@@ -29,14 +29,14 @@ RSpec.describe Phone, type: :model do
     expect(new_phone).not_to be_valid
   end
 
-  it 'requires a is_main validation to create a phone' do
-    new_phone.is_main = nil
+  it 'requires phone_kind to be included in the list' do
+    new_phone.phone_kind = 'outra coisa'
     expect(new_phone).not_to be_valid
   end
 
   it 'requires the number has not already been taken to create a phone' do
     new_phone.save!
-    invalid_number = Phone.new(contact:, number: '11112222', phone_kind: 'Casa', is_main: false)
+    invalid_number = Phone.new(contact:, number: '11112222', phone_kind: 'casa', is_main: false)
     invalid_number.save
     expect(invalid_number).not_to be_valid
   end
@@ -44,14 +44,14 @@ RSpec.describe Phone, type: :model do
   it 'creates a phone with the same number to other user' do
     new_phone.save!
     second_contact = Contact.create(user:, name: 'Marcos')
-    valid_phone = Phone.new(contact: second_contact, number: '11112222', phone_kind: 'Casa', is_main: false)
+    valid_phone = Phone.new(contact: second_contact, number: '11112222', phone_kind: 'casa', is_main: false)
     valid_phone.save
     expect(valid_phone).to be_valid
   end
 
   it 'requires the is_main is the only true value to create another phone' do
     new_phone.save!
-    invalid_phone = Phone.new(contact:, number: '33334444', phone_kind: 'Casa', is_main: true)
+    invalid_phone = Phone.new(contact:, number: '33334444', phone_kind: 'casa', is_main: true)
     invalid_phone.save
     expect(invalid_phone).not_to be_valid
   end
