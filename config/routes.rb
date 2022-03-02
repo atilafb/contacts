415f4f36b73137ed805ca4a83512d697a72000e1
Atila <atilafb77@gmail.com>
2022-03-02 13:51:29 -0300

    remove unused files, fix controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users' => 'users#index'
-  get 'users' => 'users#new'
-  post 'users' => 'users#create'
-  get 'users' => 'users#edit'
-  put 'users' => 'users#update'
-  delete 'users' => 'users#destroy'
-  get 'users/:id' => 'users#show', as: :user
 end
