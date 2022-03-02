29c7223d72109342b1259f16d35ba91e896c59aa
Atila <atilafb77@gmail.com>
2022-03-02 16:27:24 -0300

    fix tests file to implement only the task requirement, and verify the action behaviour

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,52 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
+    subject(:users) { User.all }
+    subject(:user) { User.new(name: 'Thomas', age: 32, biography: 'Born in Birmigham') }
+
     it "returns http success" do
       get users_path
+      user.save
       expect(response).to have_http_status(:success)
+      expect(users).to eq(User.all)
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/users/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/users/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/users/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/users/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
